cacb12b5fbbd9bf2cc45ae3d77355862e7127eec
Mika Leskinen <mika.leskinen@savonia.fi>
2025-02-10 13:51:29 +0200

    Add raspberry pi main loop

--- raspberry-pi/app/src/index.js
+++ raspberry-pi/app/src/index.js
@@ -1,11 +1,31 @@
+const moment = require("moment");
 const Rpi5 = require("./rpi5");
 const RandomData = require("./randomData");
 
-//
-
+// looping main function
 const main = async () => {
-  //console.log(await Rpi5.getCpuTemp());
-  console.log(await RandomData.getRandomFloat(20, 200));
+  // create data message, read values and send to backend
+
+  // init with unix milliseconds timestamp
+  // see https://en.wikipedia.org/wiki/Unix_time
+  let data = { ts: moment().valueOf(), values: {} };
+
+  // get Rpi CPU temperature
+  //data.values["cpu_temp"] = await Rpi5.getCpuTemp();
+
+  // get random float value
+  data.values["random_value"] = await RandomData.getRandomFloat(20, 200);
+
+  // output to console
+  // TODO: send to cloud backend instead
+  console.log(data);
 };
 
+// run once
 main();
+
+// run every 10 seconds
+// TODO: add proper scheduling
+setInterval(() => {
+  main();
+}, 10 * 1000);
